6ffe01ee524b50e28275b72c05c152570beadd06
zuiwuchang <zuiwuchang@gmail.com>
2022-12-13 09:48:08 +0800

    emptyObject

--- src/values.ts
+++ src/values.ts
@@ -15,3 +15,5 @@ export const neverPromise = new Promise<any>(() => { })
  * This function does nothing and can usually be used as the default handler for something
  */
 export function nopCallback(): void { }
+
+export const emptyObject = {}
